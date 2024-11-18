8a8c6f322ff83545ef3f93ecc3272f950294a736
Erik-Paulin <152278350+Erik-Paulin@users.noreply.github.com>
2024-11-18 12:50:44 +0100

    Start

--- db/seeder.rb
+++ db/seeder.rb
@@ -1,11 +1,41 @@
 require 'sqlite3'
 
 class Seeder
-
+  
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS todo')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE todos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                description TEXT)')
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO todos (name, description) VALUES ("Äpple ", "En rund frukt som finns i många olika färger.")')
+    db.execute('INSERT INTO todos (name, description) VALUES ("Päron ", "En nästan rund, men lite avläng, frukt. Oftast mjukt fruktkött.")')
+    db.execute('INSERT INTO todos (name, description) VALUES ("Banan ", "En avlång gul frukt.")')
+    db.execute('INSERT INTO todos (name, description) VALUES ("Appelsin ", "En orange rund frukt.")')
+    db.execute('INSERT INTO todos (name, description) VALUES ("Mandarin ", "En orange rund frukt.")')
+    db.execute('INSERT INTO todos (name, description) VALUES ("Tsatsuma ", "En orange rund frukt.")')
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/todos.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
+
 
 Seeder.seed!
